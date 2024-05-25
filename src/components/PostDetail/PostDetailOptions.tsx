268f72a7689d098cb0222856eade636fffaddd27
장광진 <79461880+wkdrhkdwls@users.noreply.github.com>
2024-05-25 15:10:08 +0900

    30 fix 중간코멘트 수정 (#35)
    * Fix - 회원가입 시 조합 문제 해결
    
    * Chore : usestate의 dispatch네이밍을 setState명으로 변경
    
    * Refact : 한수선언방식에서 const로 변환
    
    * Fix : zustand를 사용하지않고 session을 사용하여 usestate로 page관리하기
    
    * Fix : zustand 롤백 이후 totalpages state값으로 관리
    
    * Refact : 컴포넌트를 분리& 폴더를 통일하여 유지보수성을 높임
    
    * Refact : type폴더로 type 이전
    
    * Fix : 빌드에러 수정
    
    * Fix : type 에러 및 빌드 에러 해결
    
    * Design : 수정, 삭제 모달에 hover 추가

--- src/components/PostDetail/PostDetailOptions.tsx
+++ src/components/PostDetail/PostDetailOptions.tsx
@@ -9,8 +9,8 @@ const PostDetailOptions = ({ showOptions, toggleOptions, openModal }: PostOption
       </button>
       {showOptions && (
         <ul className="absolute flex flex-col text-left right-0 top-full mt-2 w-[112px] bg-white shadow-lg rounded-lg p-2 z-50">
-          <button className="p-2 cursor-pointer">수정</button>
-          <button className="p-2 cursor-pointer" onClick={openModal}>
+          <button className="p-2 cursor-pointer hover:bg-[#F9F9F9]">수정</button>
+          <button className="p-2 cursor-pointer hover:bg-[#F9F9F9]" onClick={openModal}>
             삭제
           </button>
         </ul>
