fe52b36a8db7f25dab88d3f43b102e400909c566
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-05-26 12:40:04 +0900

    Fix : 제거한 routes rollback

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+export const pageRoutes = {
+  main: '/',
+  login: '/login',
+  signUp: '/signup',
+  signUpComplete: '/signup-complete',
+  createPost: '/create-post',
+  detailPost: '/post/:id',
+  chart: '/chart',
+} as const;
